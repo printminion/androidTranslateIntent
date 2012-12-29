f1664104fc28cf2a31a332e76fa95a219dd4d898
@russenreaktor <cr@russenreaktor.de>
2012-12-30 00:48:19 +0100

    Update trunk/src/com/kupriyanov/android/apps/translate/Setup.java
    removed api key

--- trunk/src/com/kupriyanov/android/apps/translate/Setup.java
+++ trunk/src/com/kupriyanov/android/apps/translate/Setup.java
@@ -2,9 +2,9 @@ package com.kupriyanov.android.apps.translate;
 
 public class Setup {
 
-	public final static String API_KEY_GOOGLETRANSLATE = "AIzaSyBM0BH1YigwhkJOL5v9EssKtNOc8ead40w";
+	public final static String API_KEY_GOOGLETRANSLATE = "PUT_HERE_YOUR_API_KEY_GOOGLETRANSLATE";
 	public final static int LANGUAGES_COUNT = 4;
-	public final static String API_KEY_GOOGLEANALYTICS = "UA-3508250-5";
+	public final static String API_KEY_GOOGLEANALYTICS = "PUT_HERE_YOUR_API_KEY_GOOGLEANALYTICS";
 
 	public static boolean API_KEY_GOOGLEANALYTICS_ON = true;
 
